623c36ebdee9213f5dec5e75f5a5fdbd6458c842
oleggr <oleg.gr@outlook.com>
2021-10-29 05:33:10 +0300

    cors support added

--- backend/scheduler/app.py
+++ backend/scheduler/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from scheduler.routes import router
 
@@ -6,6 +7,14 @@ from scheduler.routes import router
 def get_application() -> FastAPI:
     application = FastAPI()
     application.include_router(router, prefix="/api")
+
+    application.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     return application
 
