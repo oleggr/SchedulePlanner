13aa439d46748647e90c20b58c8f7a54f9965b7a
oleggr <oleg.gr@outlook.com>
2021-10-29 05:53:14 +0300

    basic pathfinding implemented

--- backend/scheduler/schedule.py
+++ backend/scheduler/schedule.py
@@ -1,6 +1,10 @@
 from abc import abstractmethod
 from typing import List
 
+from pathfinding.core.diagonal_movement import DiagonalMovement
+from pathfinding.core.grid import Grid
+from pathfinding.finder.a_star import AStarFinder
+
 from scheduler.models import Strategy1Model
 
 
@@ -13,20 +17,20 @@ class Strategy1Planner(BasicPlanner):
 
     field: List[List[int]]
     end_x: int
+    finder = AStarFinder
 
     def __init__(self, strategy_data: Strategy1Model):
         self.field = strategy_data.field
         self.end_x = strategy_data.end_x
 
     def get_schedule(self):
-        # TODO: implement Lee algorithm
         best_schedule_rate = len(self.field[0]) * len(self.field)
         best_schedule = []
 
-        for line in self.field:
-            for i in range(len(line) - self.end_x):
-                start_index = i
-                end_index = i + self.end_x - 1
+        for i in range(len(self.field)):
+            for j in range(len(self.field[i]) - self.end_x):
+                start_index = (i, j)
+                end_index = (i, j + self.end_x)
                 curr_schedule, curr_rate = self.find_path(start_index, end_index)
                 if curr_rate < best_schedule_rate:
                     best_schedule = curr_schedule
@@ -34,6 +38,18 @@ class Strategy1Planner(BasicPlanner):
         return best_schedule
 
     def find_path(self, start, end):
+        grid = Grid(matrix=self.field)
+        start = grid.node(start[1], start[0])
+        end = grid.node(end[1], end[0])
+
+        route = self.finder(
+            diagonal_movement=DiagonalMovement.always,
+            time_limit=10
+        )
+        path, runs = route.find_path(start, end, grid)
+        # print('operations:', runs, 'path length:', len(path))
+        # print(grid.grid_str(path=path, start=start, end=end))
+
         schedule, curr_rate = [
             [0, 0, 0, 0, 0, 0],
             [1, 1, 1, 1, 1, 1],
