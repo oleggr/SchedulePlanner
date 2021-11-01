cfcecb906105c7e713388a592058cf019b5bba65
oleggr <oleg.gr@outlook.com>
2021-11-02 00:24:32 +0300

    utils fixed

--- backend/scheduler/utils.py
+++ backend/scheduler/utils.py
@@ -6,12 +6,13 @@ def print_arr(arr):
         print()
 
 
-def copy_2d_array_structure(arr):
+def copy_2d_array(arr, only_structure=False):
     new_arr = []
     for i in range(len(arr)):
         new_arr.append([])
-        for _ in arr[i]:
-            new_arr[i].append(None)
+        for elem in arr[i]:
+            val = None if only_structure else elem
+            new_arr[i].append(val)
     return new_arr
 
 
@@ -20,7 +21,8 @@ def get_min(arr):
 
     for elem in arr:
         if (min_v == 0 and elem) \
-                or (elem and elem < min_v):
+                or (elem and elem < min_v)\
+                or (elem == 0 and elem < min_v):
             min_v = elem
 
     return min_v
