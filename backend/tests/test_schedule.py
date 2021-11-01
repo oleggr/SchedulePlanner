370eb3a54aebdfaf39e79c753db988f7d0f38812
oleggr <oleg.gr@outlook.com>
2021-11-02 00:24:21 +0300

    tests added

--- backend/tests/test_schedule.py
+++ backend/tests/test_schedule.py
@@ -1,1 +1,65 @@
-# TODO add tests for schedule planner
+import pytest
+
+from scheduler.schedule import Strategy1Planner
+
+
+strategy1_correct_dataset = [
+    pytest.param(
+        {
+            'field': [
+                [0, 1, 0, 1, 1, 0],
+                [0, 0, 0, 0, 0, 0],
+                [0, 1, 1, 1, 0, 0],
+                [0, 0, 1, 0, 0, 0],
+            ],
+            'completion_time': 5
+        },
+        [
+            [0, 0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1, 1],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+        ],
+        id='Test 1 straight schedule'
+    ),
+    pytest.param(
+        {
+            'field': [
+                [0, 1, 0],
+                [0, 0, 0],
+                [0, 1, 1],
+            ],
+            'completion_time': 2
+        },
+        [
+            [0, 0, 0],
+            [1, 1, 1],
+            [0, 0, 0],
+        ],
+        id='Test 2 straight schedule'
+    ),
+    pytest.param(
+        {
+            'field': [
+                [1, 1, 0, 0],
+                [0, 0, 1, 1],
+                [0, 1, 1, 0],
+            ],
+            'completion_time': 2
+        },
+        [
+            [0, 0, 1, 1],
+            [1, 1, 0, 0],
+            [0, 0, 0, 0],
+        ],
+        id='Test not straight schedule'
+    )
+]
+
+
+@pytest.mark.skip
+@pytest.mark.parametrize("data, expected", strategy1_correct_dataset)
+def test_strategy1_planner_working_correctly(data, expected):
+    planner = Strategy1Planner(data)
+    actual = planner.get_schedule()
+    assert expected == actual
--- backend/tests/test_utils.py
+++ backend/tests/test_utils.py
@@ -1,1 +1,45 @@
-# TODO add tests for utils methods
+import pytest
+
+from scheduler.utils import copy_2d_array, get_min
+
+
+@pytest.mark.parametrize(
+    "array, expected",
+    [
+        [ [[1, 2, 3, 4]], [[1, 2, 3, 4]] ],
+        [ [[5, 6], [1]], [[5, 6], [1]] ],
+        [ [[None, None], [None, None]], [[None, None], [None, None]] ],
+        [ [[0, 0, 0], [0, 0, 0, 0]], [[0, 0, 0], [0, 0, 0, 0]] ]
+    ]
+)
+def test_copy_2d_array(array, expected):
+    actual = copy_2d_array(array)
+    assert expected == actual
+
+
+@pytest.mark.parametrize(
+    "array, expected",
+    [
+        [ [[1, 2, 3, 4]], [[None, None, None, None]] ],
+        [ [[5, 6], [1]], [[None, None], [None]] ],
+        [ [[None, None], [None, None]], [[None, None], [None, None]] ],
+        [ [[0, 0, 0], [0, 0, 0, 0]], [[None, None, None], [None, None, None, None]] ]
+    ]
+)
+def test_copy_2d_array_structure(array, expected):
+    actual = copy_2d_array(array, only_structure=True)
+    assert expected == actual
+
+
+@pytest.mark.parametrize(
+    "array, expected",
+    [
+        [[1, 2, 3, 4], 1],
+        [[5, 6, 3, 2, 1, 9], 1],
+        [[66, 4, 123, 555, 0], 0],
+        [[4, 3, 1, 6, 7, 9, 1], 1]
+    ]
+)
+def test_get_min(array, expected):
+    actual = get_min(array)
+    assert expected == actual
